c9a52d86045c783a44b7c02ce344cb0655d197fd
Pedro Karasudani <pedro.karasudani@hotmail.com>
2024-11-15 22:17:11 -0300

    implementation Delete

--- src/application/Main.java
+++ src/application/Main.java
@@ -32,9 +32,13 @@ public class Main {
         System.out.println("Inserted! new id = " + newSeller.getId());
 
         System.out.println("\n=== TEST 5: seller update ===");
-        seller = sellerDao.findById(1);
+        seller = sellerDao.findById(2);
         seller.setName("Martha Waine");
         sellerDao.update(seller);
         System.out.println("Update completed");
+
+        System.out.println("\n=== TEST 6: seller delete ===");
+        sellerDao.deleteById(2);
+        System.out.println("Delete completed");
     }
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -68,7 +68,9 @@ public class SellerDaoJDBC implements SellerDao {
             st.setInt(5, obj.getDepartment().getId());
             st.setInt(6, obj.getId());
 
-            st.executeUpdate();
+            int rows = st.executeUpdate();
+
+
         } catch (SQLException e) {
             throw new DbException(e.getMessage());
         }
@@ -79,6 +81,21 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public void deleteById(Integer id) {
+        PreparedStatement st = null;
+        try {
+            st = conn.prepareStatement("DELETE FROM seller WHERE Id = ?");
+            st.setInt(1, id);
+            int rows = st.executeUpdate();
+
+            if (rows == 0) {
+                throw new DbException("Error delete, id nonexistent");
+            }
+
+        } catch (SQLException e){
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+        }
     }
 
     @Override
